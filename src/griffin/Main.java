03d79fd83c3ff8d70f95d7fcf5c9ee0eb29c7520
patnjacq <yan20004@byui.edu>
2021-01-29 16:50:57 -0800

    Jacque says hello

--- src/griffin/Main.java
+++ src/griffin/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Hadassah");
+        System.out.println("Hello Hadassah, my name is Jacque");
     }
 }
