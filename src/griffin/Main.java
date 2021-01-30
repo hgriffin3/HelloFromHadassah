1d9d8b1a72ab300f9b60e145a19ab29477d632d9
chilla2 <64.aprilhill@gmail.com>
2021-01-30 14:38:05 -0700

    Added a print line.

--- src/griffin/Main.java
+++ src/griffin/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello, my name is Hadassah");
         System.out.println("Hello Hadassah, my name is Jacque");
         System.out.println("Hello Hadassah and Jacque, my name is Michael");
+        System.out.println("Hello, Hadassah, Jacque, and Michael; my name is April.");
     }
 }
